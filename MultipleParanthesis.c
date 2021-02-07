e7e2897536a8c236f8836dd9fd9c09f004cedad3
shrutighoradkar10 <75423631+shrutighoradkar10@users.noreply.github.com>
2021-02-07 15:14:36 +0530

    Multiple Paranthesis Matching Using Stack

---
+++ MultipleParanthesis.c
@@ -0,0 +1,119 @@
+#include<stdio.h>
+#include<stdlib.h>
+struct stack
+{
+    int size;
+    int top;
+    char *arr;
+};
+int isEmpty(struct stack *ptr)
+{
+    if(ptr->top==-1)
+    {
+        return 1;
+    }
+    else
+    {
+        return 0;
+    }
+}
+int isFull(struct stack *ptr)
+{
+    if(ptr->top==ptr->size-1)
+    {
+        return 1;
+    }
+    else{
+        return 0;
+    }
+}
+
+void push(struct stack *ptr,int val)
+{
+    if(isFull(ptr))
+    {
+        printf("Stack Overflow!");
+    }
+    else{
+        ptr->top++;
+        ptr->arr[ptr->top]=val;
+    }
+}
+
+char pop(struct stack *ptr)
+{
+    if(isEmpty(ptr))
+    {
+        printf("Stack Underflow!");
+        return -1;
+    }
+    else{
+        char val=ptr->arr[ptr->top];
+        ptr->top--;
+        return val;
+    }
+}
+
+int match(char a,char b)
+{
+    if(a=='{' && b=='}'){
+        return 1;
+    }
+    if(a=='(' && b==')'){
+        return 1;
+    }
+    if(a=='[' && b==']'){
+        return 1;
+    }
+    return 0;
+}
+
+int parenthesisMatch(char *exp)
+{
+    struct stack *sp;
+    sp->size=100;
+    sp->top=-1;
+    sp->arr=(char *)malloc(sp->size * sizeof(char));
+    char popped_ch;
+
+    for(int i=0; exp[i]!='\0';i++)
+    {
+        if(exp[i]=='(' || exp[i]=='{' || exp[i]=='[')
+        {
+            push(sp,exp[i]);
+        }
+        else if(exp[i]==')' || exp[i]=='}' || exp[i]==']')
+        {
+            if(isEmpty(sp))
+            {
+                return 0;
+            }
+            popped_ch=pop(sp);
+            if(!match(popped_ch,exp[i]))
+            {
+                return 0;
+            }
+        }
+    }
+    if(isEmpty(sp))
+    {
+        return 1;
+    }
+    else{
+        return 0;
+    }
+    
+}
+int main()
+{
+    char * exp="(([8]*{9}))";
+    if(parenthesisMatch(exp))
+    {
+        printf("The parenthesis is balanced");
+    }
+    else
+    {
+        printf("The parenthesis is not balanced");
+    }
+    return 0;
+}
